a5441424eaf751db0399fc24ebdcd0f795e96e9f
idrisG <idris.gasmi97@gmail.com>
2022-11-12 15:47:56 +0100

    Implementation of EmployeeDTO
    Isolate Employee use in service
    Logs enabled for security

--- add-user-app/src/main/java/com/example/userapp/controller/EmployeeController.java
+++ add-user-app/src/main/java/com/example/userapp/controller/EmployeeController.java
@@ -1,18 +1,21 @@
 package com.example.userapp.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.userapp.annotation.Log;
+import com.example.userapp.dto.EmployeeDTO;
 import com.example.userapp.exception.CustomFormException;
-import com.example.userapp.model.Employee;
 import com.example.userapp.service.EmployeeService;
 
 @RestController
@@ -22,7 +25,16 @@ public class EmployeeController {
 	 */
 	@Autowired
 	private EmployeeService employeeService;
-	
+	/**
+     * Get all usernames of employee registered in database
+     * @return
+     */
+    @Log
+    @GetMapping("/employees")
+    public ResponseEntity<List<String>> getEmployees() {      
+        return new ResponseEntity<>(employeeService.findAllUsername(), HttpStatus.CREATED);
+    }
+    
     /**
      * Create employee and register in database if employee is Valid
      * and employee.username isn't already used
@@ -34,15 +46,15 @@ public class EmployeeController {
      */
     @Log
     @PostMapping("/employees")
-    public ResponseEntity<Employee> createEmployee(@RequestBody @Valid Employee employee, BindingResult bindingResult) {
-    	employee = employeeService.createEmployee(employee);
-    	if(employee == null) {
+    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody @Valid EmployeeDTO employeeDTO, BindingResult bindingResult) {
+    	employeeDTO = employeeService.createEmployee(employeeDTO);
+    	if(employeeDTO == null) {
     		bindingResult.reject("username.invalid", "Username already used ! ");
     		throw new CustomFormException(HttpStatus.BAD_REQUEST, bindingResult);
     	}
         if (bindingResult.hasErrors()) {
         	throw new CustomFormException(HttpStatus.BAD_REQUEST, bindingResult);
         }        
-        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+        return new ResponseEntity<>(employeeDTO, HttpStatus.CREATED);
     }
 }
--- add-user-app/src/main/java/com/example/userapp/service/EmployeeService.java
+++ add-user-app/src/main/java/com/example/userapp/service/EmployeeService.java
@@ -3,6 +3,7 @@ package com.example.userapp.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.userapp.dto.EmployeeDTO;
 import com.example.userapp.model.Employee;
 import com.example.userapp.repository.EmployeeRepository;
 
@@ -20,6 +22,8 @@ import com.example.userapp.repository.EmployeeRepository;
 public class EmployeeService implements UserDetailsService {
 	private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
 	/**
 	 * Constructor, Initialize database with list of employee since database is empty when launching app
 	 * @param employeeRepository
@@ -52,43 +56,73 @@ public class EmployeeService implements UserDetailsService {
 	 * @return saved employee if username not used, null otherwise
 	 * @see {@link org.springframework.data.repository.CrudRepository#save(Object) CrudRepository.save(Object)}
 	 */
-	public Employee createEmployee(Employee employee) {
-		if(findByUsername(employee.getUsername())!=null) {
+	public EmployeeDTO createEmployee(EmployeeDTO employeeDTO) {
+		if(findByUsername(employeeDTO.getUsername())!=null) {
 			return null;
 		}
-		employee.setPassword(encoder().encode(employee.getPassword()));
-		return employeeRepository.save(employee);
+		employeeDTO.setPassword(encoder().encode(employeeDTO.getPassword()));
+		return entityToDTO(employeeRepository.save(dtoToEntity(employeeDTO)));
 	}
 	
 	/**
 	 * find all instance of Employee in database,
 	 * @see  {@link org.springframework.data.repository.CrudRepository#findAll() CrudRepository.findAll()}
-	 * @return
+	 * @return List of the usernames
 	 */
-	public Iterable<Employee> findAll(){
-		return employeeRepository.findAll();
+	public List<String> findAllUsername(){
+		List<String> listEmployeesUsername = new ArrayList<>();
+		for(Employee e : employeeRepository.findAll()) {
+			listEmployeesUsername.add(e.getUsername());
+		}
+		return listEmployeesUsername;
 	}
 	/**
 	 * Find Employee by username
 	 * @param username
 	 * @return Employee if found, null otherwise
 	 */
-	public Employee findByUsername(String username){
-		return employeeRepository.findByUsername(username);
+	public EmployeeDTO findByUsername(String username){
+		return entityToDTO(employeeRepository.findByUsername(username));
 	}
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Employee employee = employeeRepository.findByUsername(username);
-		if(employee == null) {
+		EmployeeDTO employeeDTO = findByUsername(username);
+		if(employeeDTO == null) {
 			throw new UsernameNotFoundException(username);
 		}
-		return User.withUsername(username).password(employee.getPassword()).roles(employee.getRole()).build();
+		return User.withUsername(username).password(employeeDTO.getPassword()).roles(employeeDTO.getRole()).build();
 	}
 	
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
 	}
+    /**
+     * Method that transform an entity to a dto
+     * 
+     * @param employee
+     * @return
+     */
+    private EmployeeDTO entityToDTO(Employee employee) {
+        if (employee == null) {
+            return null;
+        }
+        EmployeeDTO employeeDTO = modelMapper.map(employee, EmployeeDTO.class);
+        return employeeDTO;
+    }
 
+    /**
+     * Method that transform a dto to an entity
+     * 
+     * @param employeeDTO
+     * @return
+     */
+    private Employee dtoToEntity(EmployeeDTO employeeDTO) {
+        if (employeeDTO == null) {
+            return null;
+        }
+        Employee employee = modelMapper.map(employeeDTO, Employee.class);
+        return employee;
+    }
 }
